9c4809724c5246db2d13ccda292e10ebeae35857
정소이 <57484815+soiejung@users.noreply.github.com>
2023-05-19 09:35:50 +0900

    [D3] Title: 프리셀 통계, Time: 445 ms, Memory: 64,888 KB -BaekjoonHub

--- SWEA/D3/12051. 프리셀 통계/프리셀 통계.py
+++ SWEA/D3/12051. 프리셀 통계/프리셀 통계.py
@@ -5,14 +5,16 @@ for test_case in range(1, T + 1):
     
     N, p_d, p_g  = map(int,input().split())
     
-    flag = 0
+    flag = 1
     if p_d != 0 and p_g == 0:
         flag = 0
     elif p_d != 100 and p_g == 100:
         flag = 0   
     else:
         for n in range(1, N+1):
-            if (n * p_d) % 100 == 0:
+            if (n * p_d) % 100 != 0:
+                flag = 0
+            else:
                 flag = 1
                 break
     
