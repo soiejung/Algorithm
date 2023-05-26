feb82b13690d41a3c774b1cc042fb07d50963d15
정소이 <57484815+soiejung@users.noreply.github.com>
2023-05-26 17:18:20 +0900

    [level 2] Title: 전화번호 목록, Time: 95.88 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,18 +1,10 @@
 def solution(phone_book):
     answer = True
-    size = len(phone_book)
-    hashDict={}
     
-    for i in phone_book:
-        hashDict[i] = 1
-    
-    for number in phone_book:
-        jubdoo= ""  
-        for k in number:
-            jubdoo+=k
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+                return False
+            
+    return True
 
-            if jubdoo in hashDict and jubdoo !=number:
-                answer = False
-
-      
-    return answer
