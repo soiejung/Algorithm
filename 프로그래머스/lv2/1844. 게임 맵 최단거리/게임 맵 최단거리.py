fe519db34c98ab1ece0da0205a1d392aed82eb86
정소이 <57484815+soiejung@users.noreply.github.com>
2023-05-27 00:25:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.93 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,32 +1,36 @@
 def solution(maps):
     from collections import deque
+    
     answer = 0
     n = len(maps)
     m = len(maps[0])
-    visited = [[False] * m for _ in range(n)]
+    visited = [[0] * m for _ in range(n)]
     q = deque()
     q.append((0,0))
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    dx = [ 0, 0, 1, -1]
+    dy = [-1, 1, 0, 0]
     
-    visited[0][0] = True
+    visited[0][0] = 1
+
     
     while q:
-        y, x = q.popleft()
+        x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if 0<= nx< m and 0<= ny< n and maps[ny][nx] == 1:
-                if not visited[ny][nx]:
-                    visited[ny][nx] = True
-                    q.append((ny,nx))
-                    maps[ny][nx] = maps[y][x] + 1
+            if 0<= nx< n and 0<= ny< m and maps[nx][ny] == 1:
+                
+                if not visited[nx][ny]:
+                    visited[nx][ny] = 1
+                    q.append((nx,ny))
+                    maps[nx][ny] = maps[x][y] + 1
+        
                     
     if maps[n-1][m-1] == 1:
+        
         return -1
+    
+    else:    
         
-    else:
         return maps[n-1][m-1]
-        
-    return answer
