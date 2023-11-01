b2ae836c90c02496a3515033fe343a510685986b
정소이 <57484815+soiejung@users.noreply.github.com>
2023-11-01 10:38:51 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,22 +1,15 @@
-import sys
-input = sys.stdin.readline
-N = int(input())
+T = int(input())
 lst = list(map(int, input().split()))
 
-count = 0
+answer = 0
 for l in lst:
     flag = 1
-    if l != 1:
-        for i in range(2,l):
-            if l % i == 0:
-                flag = 0
-                break
-    
-    else:
-        flag = 0
-        
-    if flag:
-        count += 1
-    
+    for i in range(2, int(l**0.5)+1):
+        if l%i == 0:
+            flag = 0
 
-print(count)
+    if flag and l != 1:
+        answer += 1
+
+
+print(answer)
