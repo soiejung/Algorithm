0c634c1753000b780ff6fb1ee7c13942b48e828f
정소이 <soiejung@naver.com>
2023-12-26 14:37:00 +0900

    2023.12.26

--- SQL/leetcode_620.sql
+++ SQL/leetcode_620.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-
+# https://leetcode.com/problems/not-boring-movies/description/
 select * from cinema
 where description != 'boring'
 and mod(ID,2) = 1 # 짝수, 홀수 구하는 코드
