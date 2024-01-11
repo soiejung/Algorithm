f09695d41a0de6856edc87876bd4e95dc52014d9
정소이 <57484815+soiejung@users.noreply.github.com>
2024-01-11 22:24:05 +0900

    [level Lv.1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,26 @@
+def solution(data, ext, val_ext, sort_by):
+    answer = []
+    idx = 0
+    if ext == "code":
+        idx = 0
+    elif ext == "date":
+        idx = 1
+    elif ext == "maximum":
+        idx = 2
+    else:
+        idx = 3
+        
+    for d in data:
+        if d[idx] < val_ext:
+            answer.append(d)
+    
+    if sort_by == "code":
+        answer.sort(key=lambda x:(x[0],x[1],x[2],x[3]))
+    elif sort_by == "date":
+        answer.sort(key=lambda x:(x[1],x[0],x[2],x[3]))
+    elif sort_by == "maximum":
+        answer.sort(key=lambda x:(x[2],x[0],x[1],x[3]))
+    else:
+        answer.sort(key=lambda x:(x[3],x[0],x[1],x[2]))
+        
+    return answer
