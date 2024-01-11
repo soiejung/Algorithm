cad6765467cf507d1e0691046757b180459072db
정소이 <57484815+soiejung@users.noreply.github.com>
2024-01-11 19:07:41 +0900

    2024.01.11

---
+++ CodeApp/프로그래머스/PCCE 기출문제 9번_이웃한 칸.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/250125
+
+def solution(board, h, w):
+    answer = 0
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    for i in range(4):
+        x = h + dx[i]
+        y = w + dy[i]
+        
+        if 0 <= x < len(board) and 0 <= y < len(board[x]):
+            if board[h][w] == board[x][y]:
+                answer += 1
+    return answer
