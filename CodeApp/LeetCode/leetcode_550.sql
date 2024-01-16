78dea041b58b7183342d200a3c7405a281b0eabc
정소이 <57484815+soiejung@users.noreply.github.com>
2024-01-16 15:03:33 +0900

    2024.01.11
    2024.01.16 - 수정

--- CodeApp/LeetCode/leetcode_550.sql
+++ CodeApp/LeetCode/leetcode_550.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
 # https://leetcode.com/problems/game-play-analysis-iv/ 
-
+# prev_id, prev_date 수정 (ORDER BY에 event_date 추가)
 SELECT ROUND(COUNT(player_id) /(SELECT COUNT(DISTINCT player_id) FROM Activity),2) as fraction
 FROM 
 (
     SELECT player_id, 
         event_date,
         RANK() OVER (PARTITION BY player_id ORDER bY event_date) AS rnk,
-        LAG(player_id) OVER (ORDER BY player_id) AS prev_id,
-        LAG(event_date) OVER (ORDER BY player_id) AS prev_date
+        LAG(player_id) OVER (ORDER BY player_id, event_date) AS prev_id,
+        LAG(event_date) OVER (ORDER BY player_id, event_date) AS prev_date
     FROM Activity
 ) A
 WHERE rnk = 2
