5fb1d87e6cc28663f3e0d611cad6cd91c8636036
정소이 <57484815+soiejung@users.noreply.github.com>
2024-01-21 14:08:37 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.18 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,18 @@
+def solution(number, limit, power):
+    answer = 0
+    dp = [0 for _ in range(number+1)]
+    dp[1] = 1
+    for i in range(2, number+1):
+        for j in range(1, min(number//i+1, i)):
+            dp[i*j] += 2
+            
+    for i in range(2, int(number**(1/2))+1):
+        dp[i**2] += 1
+    
+    for d in dp:
+        if d > limit:
+            answer += power
+        else:
+            answer += d
+            
+    return answer
