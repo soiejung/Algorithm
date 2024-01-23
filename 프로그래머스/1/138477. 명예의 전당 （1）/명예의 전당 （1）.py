5ec0c61592894d8c8d76961df46e1838d87f8c0f
정소이 <57484815+soiejung@users.noreply.github.com>
2024-01-23 15:15:39 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,22 @@
+import heapq
+def solution(k, score):
+    answer = []
+    
+    lst = []
+    lst.append(score[0])
+    answer.append(score[0])
+    for i in range(1,len(score)):
+        heapq.heapify(lst)
+        min_ = lst[0]
+        if len(lst) == k:
+            if score[i] > min_:
+                heapq.heappop(lst)
+                heapq.heappush(lst,score[i])
+                min_ = lst[0]
+        elif len(lst) < k:
+            heapq.heappush(lst,score[i])
+            min_ = lst[0]
+        answer.append(min_)
+            
+        
+    return answer
