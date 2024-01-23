3942ad9ddbc2b0ecc7ff7643d1a03e039acce44c
정소이 <57484815+soiejung@users.noreply.github.com>
2024-01-23 15:17:25 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,15 @@
+def solution(name, yearning, photo):
+    answer = []
+    dic = {}
+    
+    for i in range(len(name)):
+        dic[name[i]] = yearning[i]
+
+    
+    for p in photo:
+        sum_ = 0
+        for pp in p:
+            if pp in dic.keys():
+                sum_ += dic[pp]
+        answer.append(sum_)
+    return answer
