136465b5666fbdbe097078a9a512cbe3845ccf3c
정소이 <57484815+soiejung@users.noreply.github.com>
2024-02-23 16:06:30 +0900

    약수 구하는 방식이 잘못되었는데 정답이라 고침

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -5,13 +5,12 @@ def solution(left, right):
     dp[1] = 1
     
     for i in range(2, right + 1):
-        for j in range(1, min(right//i,i)):
+        for j in range(1, min(right//i+1,i)):
             dp[i*j] += 2
     
     for i in range(2, int(right ** (1/2))+1):
         dp[i*i] += 1
     
-    print(dp)
     for i in range(left, right+1):
         if dp[i] %2 == 0:
             answer += i
