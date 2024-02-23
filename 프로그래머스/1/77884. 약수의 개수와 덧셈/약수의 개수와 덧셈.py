e3b8f063dd62c6eac36611af1b69c09da4346375
정소이 <57484815+soiejung@users.noreply.github.com>
2024-02-23 16:05:03 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.37 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -4,14 +4,14 @@ def solution(left, right):
     dp = [0 for _ in range(right+1)]
     dp[1] = 1
     
-    for i in range(2, right+1):
-        for j in range(1, min(i, right//i)):
+    for i in range(2, right + 1):
+        for j in range(1, min(right//i,i)):
             dp[i*j] += 2
     
-    for i in range(2, int(right**(1/2))+1):
+    for i in range(2, int(right ** (1/2))+1):
         dp[i*i] += 1
     
-    
+    print(dp)
     for i in range(left, right+1):
         if dp[i] %2 == 0:
             answer += i
