86ced1c227dd33ce499dd8797cb1a369253c50e8
정소이 <57484815+soiejung@users.noreply.github.com>
2024-03-31 22:08:27 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -19,34 +19,35 @@ def solution(n):
     answer = ''
     lst = deque(func(n))
     
-    for i in range(1,len(lst)):
-        if lst[i] == 0:
-            if lst[i-1] == 1:
-                lst[i-1] = 0
+    while 0 in lst:
+        for i in range(1,len(lst)):
+            if lst[i] == 0:
+                if lst[i-1] == 1:
+                    lst[i-1] = 0
 
-            elif lst[i-1] == 2:
-                lst[i-1] = 1
+                elif lst[i-1] == 2:
+                    lst[i-1] = 1
 
-            elif lst[i-1] == 4:
-                lst[i-1] = 2
-            lst[i] = 4
+                elif lst[i-1] == 4:
+                    lst[i-1] = 2
+                lst[i] = 4
 
-    for i in range(len(lst)-1, 0 ,-1):
+        for i in range(len(lst)-1, 0 ,-1):
 
-        if lst[i] == 0:
-            if lst[i-1] == 1:
-                lst[i-1] = 0
+            if lst[i] == 0:
+                if lst[i-1] == 1:
+                    lst[i-1] = 0
 
-            elif lst[i-1] == 2:
-                lst[i-1] = 1
+                elif lst[i-1] == 2:
+                    lst[i-1] = 1
 
-            elif lst[i-1] == 4:
-                lst[i-1] = 2
-            lst[i] = 4
+                elif lst[i-1] == 4:
+                    lst[i-1] = 2
+                lst[i] = 4
 
-    if lst[0] == 0:
-        lst.popleft()
-    
+        if lst[0] == 0:
+            lst.popleft()
+
     for l in lst:
         answer += str(l)
     
