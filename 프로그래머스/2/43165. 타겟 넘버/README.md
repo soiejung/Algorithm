fee79d650cb92f73dc071fb84bade680ae6dab65
정소이 <57484815+soiejung@users.noreply.github.com>
2024-04-02 20:44:25 +0900

    [level 2] Title: 타겟 넘버, Time: 3.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 3.04 ms
+메모리: 10.3 MB, 시간: 3.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 02일 19:55:04
+2024년 04월 02일 20:44:22
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,16 @@
 def solution(numbers, target):
-
+    answer = 0
+    
     def cal(idx,total):
         nonlocal answer
+        
         if idx == len(numbers)-1:
-            if total == target:
+            if target == total:
                 answer += 1
             return
-        
-        cal(idx+1, total + numbers[idx])
+    
+        cal(idx+1,total + numbers[idx])
         cal(idx+1, total - numbers[idx])
-        
-    answer = 0
+    
     cal(-1,0)
-        
     return answer
